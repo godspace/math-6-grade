3353b2403614276a2f211576a1c75cf7ea5f51d6
Sergey Kozlov <kozlov.stereo@yandex.ru>
2026-02-09 22:21:26 +0300

    Аналитика

---
+++ assets/analytics.js
@@ -0,0 +1,19 @@
+// Файл: assets/analytics.js
+
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106746549', 'ym');
+
+ym(106746549, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: "dataLayer",
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce: true,
+    trackLinks: true
+});
